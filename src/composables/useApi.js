7fe1a039394a68ca151c6f4ed9365148f6d5fb06
Konstantin Khomenko <kostya5599@gmail.com>
2022-01-30 10:55:06 +0200

    use api

---
+++ src/composables/useApi.js
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+
+export default function useApi(getApiData) {
+  const loading = ref(false);
+  const error = ref(false);
+  const response = ref();
+
+  const callApi = async () => {
+    try {
+      response.value = await getApiData();
+    } catch (e) {
+      error.value = true;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  return { callApi, loading, error, response };
+}
