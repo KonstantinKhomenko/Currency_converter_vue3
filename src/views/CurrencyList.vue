a31f4b1a30ab971b0d5f8d3d632d0e84ec35e3b8
Konstantin Khomenko <kostya5599@gmail.com>
2022-01-30 10:55:27 +0200

    currency list

--- src/views/CurrencyList.vue
+++ src/views/CurrencyList.vue
@@ -1,10 +1,53 @@
 <template>
   <div>CurrencyList</div>
+  <div>{{ filteredCurrencies }}</div>
 </template>
 
 <script>
+import { computed } from "vue";
+import axios from "@/plugins/axios";
+import useApi from "@/composables/useApi";
+import {
+  baseCurrencyCode,
+  displayedCurrencies,
+} from "@/config/currency.config";
+
 export default {
   name: "CurrencyList",
+
+  setup() {
+    const { callApi, response: rateList } = useApi(async () => {
+      const { rates } = await axios.get(
+        `/exchange-rates?currency=${baseCurrencyCode.value}`
+      );
+
+      return rates;
+    });
+
+    callApi();
+
+    console.log(rateList.value);
+
+    const filteredCurrencies = computed(() => {
+      return Object.entries(rateList.value ?? {}).reduce(
+        (acc, [currency, rate]) => {
+          if (displayedCurrencies.includes(currency)) {
+            acc[currency] = rate;
+
+            return acc;
+          }
+        },
+        {}
+      );
+    });
+
+    console.log(filteredCurrencies.value);
+
+    return {
+      rateList,
+      filteredCurrencies,
+    };
+  },
 };
 </script>
 
