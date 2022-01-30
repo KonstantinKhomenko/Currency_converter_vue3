02bc21f4a0506830f43a67759a5fea9488a8d826
Konstantin <kostya5599@gmail.com>
2022-01-30 17:56:05 +0200

    base currency page

--- src/views/CurrencyBase.vue
+++ src/views/CurrencyBase.vue
@@ -1,11 +1,46 @@
 <template>
-  <div>CurrencyBase</div>
+  <div class="base-currency">
+    <h2>Base currency</h2>
+
+    <el-select
+      v-model="baseCurrencyCode"
+      placeholder="Select currency"
+      size="large"
+    >
+      <el-option
+        v-for="(code, index) in displayedCurrencies"
+        :key="index"
+        :label="code"
+        :value="code"
+      >
+      </el-option>
+    </el-select>
+  </div>
 </template>
 
 <script>
+import { baseCurrencyCode, displayedCurrencies } from "@/config/currency";
+import { ElSelect, ElOption } from "element-plus";
+
 export default {
   name: "CurrencyBase",
+
+  components: {
+    ElSelect,
+    ElOption,
+  },
+
+  setup() {
+    return {
+      baseCurrencyCode,
+      displayedCurrencies,
+    };
+  },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.base-currency {
+  padding: 1.5rem;
+}
+</style>
