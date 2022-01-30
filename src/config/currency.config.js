919531650f9846fc92ad36fd58a98d08433ff1b9
Konstantin Khomenko <kostya5599@gmail.com>
2022-01-30 10:54:36 +0200

    currency config

---
+++ src/config/currency.config.js
@@ -0,0 +1,16 @@
+import { ref } from "vue";
+
+export const baseCurrencyCode = ref("UAH");
+
+export const displayedCurrencies = [
+  "UAH",
+  "USD",
+  "EUR",
+  "GBP",
+  "JPY",
+  "BTC",
+  "ETH",
+  "USDT",
+  "RUB",
+  "CAD",
+];
